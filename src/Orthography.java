dc2bab5eb2c48750a41373f954fb39b29c4e61a1
Kanat Mussilim <kanat_aljan@mail.ru>
2022-10-22 14:14:01 +0600

    added answer

--- src/Orthography.java
+++ src/Orthography.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class Orthography {
-    https://acmp.ru/index.asp?main=task&id_task=940
+//    https://acmp.ru/index.asp?main=task&id_task=940
+
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+
+        StringBuilder sb = new StringBuilder(scanner.next().toUpperCase());
+
+       System.out.println( sb.deleteCharAt(a-1));
+    }
 }
